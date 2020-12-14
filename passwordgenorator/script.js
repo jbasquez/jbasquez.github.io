9da4a662c20d5e5e86d7e083803a81b1d5f23cdd
Joe Basquez <jybasquez@yahoo.com>
2020-12-14 17:04:11 -0500

    random number function

--- passwordgenorator/script.js
+++ passwordgenorator/script.js
@@ -1,6 +1,9 @@
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random()*26)+65);
+}
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random()*26)+97);
 }
-function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random()*26)+65);
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random()*10)+48);
 }
