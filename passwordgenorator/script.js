ee78a40d2b00b9c20da63f219bfabaf34a698c5f
Joe Basquez <jybasquez@yahoo.com>
2020-12-14 17:02:08 -0500

    added function random upper

--- passwordgenorator/script.js
+++ passwordgenorator/script.js
@@ -1,3 +1,6 @@
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random()*26)+97);
 }
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random()*26)+65);
+}
