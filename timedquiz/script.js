e7fb092e00533cc11a323802b2cb71c98f6d7457
Joe Basquez <jybasquez@yahoo.com>
2020-12-27 09:11:26 -0500

    made index for rado questions

--- timedquiz/script.js
+++ timedquiz/script.js
@@ -1,16 +1,37 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+let shuffledQuestions, currentQuestionIndex
+
 startButton.addEventListener('click', startGame)
 
 function startGame() {
-    console.log('started')
     startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
-console.log(startGame);
-function setNextQuestion() {
 
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+}
+
 function selectAnswer() {
 
 }
+
+const questions = [
+    {
+        question: 'If is the same as true as else is the same as?',
+        answers: [
+            {text: 'true', correct: true },
+            {text: 'maybe', correct: false }
+        ]
+    }
+]
