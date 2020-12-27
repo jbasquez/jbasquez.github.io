3d871e36b20e846496621b168d8a4de0ab2fe253
Joe Basquez <jybasquez@yahoo.com>
2020-12-27 08:42:58 -0500

    fixed start button added in defer to src code on jave

--- timedquiz/script.js
+++ timedquiz/script.js
@@ -1,9 +1,16 @@
-function startGame(){
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
+startButton.addEventListener('click', startGame)
 
+function startGame() {
+    console.log('started')
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
 }
+console.log(startGame);
 function setNextQuestion() {
 
 }
 function selectAnswer() {
-    
+
 }
