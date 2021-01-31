8f5c617a78671bcf964a448d648708742c6d0c84
Joe Basquez <jybasquez@yahoo.com>
2021-01-31 18:19:56 -0500

    readMe generator has been started

---
+++ readMeGenerator/Develop/index.js
@@ -0,0 +1,13 @@
+// TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+// TODO: Create an array of questions for user input
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
