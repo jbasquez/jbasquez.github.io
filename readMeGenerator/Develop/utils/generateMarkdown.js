8e33e51676cdebdd7f02db6058b42be3f9904995
Joe Basquez <jybasquez@yahoo.com>
2021-02-08 07:17:30 -0500

    started license badge

--- readMeGenerator/Develop/utils/generateMarkdown.js
+++ readMeGenerator/Develop/utils/generateMarkdown.js
@@ -1,11 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -14,13 +17,13 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
     return `
-        # ${data.title}
-        <h2>About:</h2>
-        *repo: ${data.repoName}
-        *discription: ${data.discription}
-        *contributors: ${data.contributors}
-        *license: ${data.license}
-        *instilation: ${data.instilation}
+        # Project Title: ${data.title}
+        ## About:
+        - repo: ${data.repoName}
+        - discription: ${data.discription}
+        - contributors: ${data.contributors}
+        - license: ${data.license}
+        - instilation: ${data.instilation}
         *ready: ${data.ready}
         <h2># Contact</h2>
         *username: ${data.username}
