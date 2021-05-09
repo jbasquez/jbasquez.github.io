347d9eb23d2d9bddb88688a718202eda70d0f0bc
Joe Basquez <jybasquez@yahoo.com>
2021-05-09 09:29:14 -0400

    project skeleton

--- reactapp/src/components/project.js
+++ reactapp/src/components/project.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 function ProjectCard(props) {
     return(
-
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> {props.name}
+          </li>
+          <li>
+            <strong>About:</strong> {props.about}
+          </li>
+          <li>
+            <strong>Active Link:</strong> {props.active}
+          </li>
+          <li>
+            <strong>Repo:</strong> {props.repo}
+          </li>
+        </ul>
+      </div>
+    </div>
     );
 }
 export default ProjectCard;
