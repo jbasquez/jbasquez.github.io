0afd3b1a4e94e73d17e4dc9297b8550fffa6d7ea
Joe Basquez <jybasquez@yahoo.com>
2021-05-09 08:57:12 -0400

    added in skeleton for footer

---
+++ reactapp/src/components/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Footer() {
+    return(
+        <footer className="fixed-bottom">
+            <div className="footerText">Made by: Joe Basquez</div>
+        </footer>
+    );
+}
+
+export default Footer;
