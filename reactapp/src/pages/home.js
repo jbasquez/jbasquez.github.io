6665c1fc4bec1583411638e5b03629726b75dc43
Joe Basquez <jybasquez@yahoo.com>
2021-05-11 07:18:20 -0400

    no bullet style class added and used

--- reactapp/src/pages/home.js
+++ reactapp/src/pages/home.js
@@ -20,7 +20,7 @@ function Home(){
 
     <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 mainHome">
       <h3>Activities</h3>
-      <ul>
+      <ul className="noBullet">
         <li>Rock climbing</li>
         <li>Software development</li>
         <li>MMA</li>
