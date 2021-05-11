62a1f32bb6d379ddee56a34b29732ef029b93126
Joe Basquez <jybasquez@yahoo.com>
2021-05-10 22:49:46 -0400

    styling

--- reactapp/src/pages/projects.js
+++ reactapp/src/pages/projects.js
@@ -1,10 +1,12 @@
 import ProjectCard from '../components/project';
 import project from '../project.json';
-
+import "../scss/style.css";
 
 function Projects() {
     return (
-      <div className="projects col-lg-6 col-md-6 col-sm-6 col-xs-12">
+      <div>
+        <div className="card col-md-4 col-sm-12 col-xs-12 container-fluid">
+          <div>
         <ProjectCard 
           id={project[0].id}
           name={project[0].name}
@@ -13,6 +15,38 @@ function Projects() {
           active={project[0].active}
           repo={project[0].repo}
         />
+  </div>
+  <div>
+        <ProjectCard 
+          id={project[0].id}
+          name={project[0].name}
+          about={project[0].about}
+          image={project[2].image}
+          active={project[0].active}
+          repo={project[0].repo}
+        />
+        </div>
+        </div>
+        <div className="card col-md-4 col-sm-12 col-xs-12 container-fluid">
+        <ProjectCard 
+          id={project[0].id}
+          name={project[0].name}
+          about={project[0].about}
+          image={project[2].image}
+          active={project[0].active}
+          repo={project[0].repo}
+        />
+        </div>
+        <div className="card col-md-4 col-sm-12 col-xs-12 container-fluid">
+        <ProjectCard 
+          id={project[0].id}
+          name={project[0].name}
+          about={project[0].about}
+          image={project[2].image}
+          active={project[0].active}
+          repo={project[0].repo}
+        />
+        </div>
       </div>
     );
   }
