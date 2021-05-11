b842c243aa0bf1e8a6ab2f463e2f2df4be8b5e76
Joe Basquez <jybasquez@yahoo.com>
2021-05-11 07:14:18 -0400

    expand small

--- reactapp/src/components/header.js
+++ reactapp/src/components/header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 function NavBar(){
     return(
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-sm navbar-light bg-light">
       <div className="navbar-brand"></div>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
