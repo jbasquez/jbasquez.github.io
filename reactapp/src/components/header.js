9f71ef55c87137b8aea0d88eb47cf2cc3cf87830
Joe Basquez <jybasquez@yahoo.com>
2021-05-13 13:21:04 -0400

    finishing navbar

--- reactapp/src/components/header.js
+++ reactapp/src/components/header.js
@@ -7,7 +7,7 @@ function NavBar(){
       <div className="navbar-brand"></div>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
-            <a className="nav-link" href="/">Home</a>
+            <Link to="/" className="nav-link">Home</Link>
             <Link to="/projects" className="nav-link">Portfolio</Link>
             <Link to="/contactInfo" className="nav-link">Contact</Link>
             <a className="nav-link" href="https://docs.google.com/document/d/19_8j402m4p8IEDOEZpv6RuhytvTNt7uwzL1mzieTZ-I/edit?usp=sharing">Resume</a>
