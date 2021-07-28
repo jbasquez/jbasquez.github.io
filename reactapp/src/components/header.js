1cd206e3c839dc01c523a4eb283e48e62ca07c47
Joe Basquez <jybasquez@yahoo.com>
2021-07-28 16:22:38 -0400

    navbar sized for all screens and positioned

--- reactapp/src/components/header.js
+++ reactapp/src/components/header.js
@@ -4,10 +4,10 @@ import React from 'react';
 function NavBar(){
     return(
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/">Home</a>
-        <a class="navbar-brand" href="/projects">Portfolio</a>
-        <a class="navbar-brand" href="/contactInfo">Contact</a>
-        <a class="navbar-brand" href="https://docs.google.com/document/d/1JRJTE-A6u941O3F4mi2I1YtNntQPzBcEoe0PJGPR8Yo/edit?usp=sharing">Resume</a>
+        <a class="navbar-brand col-lg-1 col-md-1 col-sm-12 col-xs-12" href="/">Home</a>
+        <a class="navbar-brand col-lg-1 col-md-1 col-sm-12 col-xs-12" href="/projects">Portfolio</a>
+        <a class="navbar-brand col-lg-1 col-md-1 col-sm-12 col-xs-12" href="/contactInfo">Contact</a>
+        <a class="navbar-brand col-lg-1 col-md-1 col-sm-12 col-xs-12" href="https://docs.google.com/document/d/1JRJTE-A6u941O3F4mi2I1YtNntQPzBcEoe0PJGPR8Yo/edit?usp=sharing">Resume</a>
       </nav>
     )
 }
