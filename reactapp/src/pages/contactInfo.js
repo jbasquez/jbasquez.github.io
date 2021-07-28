4d4d8c0d4c8c592ee1e8e5d91aaa955e5ef3fbe2
Joe Basquez <jybasquez@yahoo.com>
2021-07-28 16:13:46 -0400

    mobile responsive fixed for contact card

--- reactapp/src/pages/contactInfo.js
+++ reactapp/src/pages/contactInfo.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ContactCard(){
     return(
         <div className="sizing">
-            <div className="max">
+            <div className="col-lg-12 col-md-4 col-sm-12 col-xs-12">
                 <Footer />
             </div>
         </div>
