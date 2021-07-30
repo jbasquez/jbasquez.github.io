552462e3757bec35e36399ce1b59af9789c269c4
Joe Basquez <jybasquez@yahoo.com>
2021-07-29 20:39:55 -0400

    new resume link

--- reactapp/src/components/header.js
+++ reactapp/src/components/header.js
@@ -7,7 +7,7 @@ function NavBar(){
         <a class="navbar-brand col-lg-1 col-md-1 col-sm-12 col-xs-12" href="/">Home</a>
         <Link to="/projects" className="navbar-brand col-lg-1 col-md-1 col-sm-12 col-xs-12">Portfolio</Link>
         <Link to="/contactInfo" className="navbar-brand col-lg-1 col-md-1 col-sm-12 col-xs-12">Contact</Link>
-        <a class="navbar-brand col-lg-1 col-md-1 col-sm-12 col-xs-12" href="https://docs.google.com/document/d/1JRJTE-A6u941O3F4mi2I1YtNntQPzBcEoe0PJGPR8Yo/edit?usp=sharing">Resume</a>
+        <a class="navbar-brand col-lg-1 col-md-1 col-sm-12 col-xs-12" href="https://docs.google.com/document/d/1038_sdzmqFadOf_M20GMJyJ3LrQY_zHgHVfo0ud0V20/edit?usp=sharing">Resume</a>
       </nav>
     )
 }
