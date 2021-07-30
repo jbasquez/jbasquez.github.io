574315029a90c485f67a58c101e08558dd6f35c3
Joe Basquez <jybasquez@yahoo.com>
2021-07-29 20:34:50 -0400

    debugging navbar for live apllication

--- reactapp/src/components/header.js
+++ reactapp/src/components/header.js
@@ -1,12 +1,12 @@
 import React from 'react';
-// import {Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 function NavBar(){
     return(
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand col-lg-1 col-md-1 col-sm-12 col-xs-12" href="/">Home</a>
-        <a class="navbar-brand col-lg-1 col-md-1 col-sm-12 col-xs-12" href="/projects">Portfolio</a>
-        <a class="navbar-brand col-lg-1 col-md-1 col-sm-12 col-xs-12" href="/contactInfo">Contact</a>
+        <Link to="/projects" className="navbar-brand col-lg-1 col-md-1 col-sm-12 col-xs-12">Portfolio</Link>
+        <Link to="/contactInfo" className="navbar-brand col-lg-1 col-md-1 col-sm-12 col-xs-12">Contact</Link>
         <a class="navbar-brand col-lg-1 col-md-1 col-sm-12 col-xs-12" href="https://docs.google.com/document/d/1JRJTE-A6u941O3F4mi2I1YtNntQPzBcEoe0PJGPR8Yo/edit?usp=sharing">Resume</a>
       </nav>
     )
