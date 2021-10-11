ab125fb7e7b60293060ac30135b6230d26a75370
Joe Basquez <jybasquez@yahoo.com>
2021-10-11 17:21:24 -0400

    updated some alt

--- reactapp/src/components/icons.js
+++ reactapp/src/components/icons.js
@@ -8,8 +8,8 @@ function SocialMediaIcons(){
         <div ClassName="bg">
             <div className="noBullet bg">
             <a href='https://www.linkedin.com/in/joseph-basquez-a1ab6a1b5/' target="_blank" rel="noopener noreferrer"><img src={linkedIn} className="logo" alt="icon for linked in"/></a>
-            <a href='https://github.com/jbasquez' target="_blank" rel="noopener noreferrer"><img src={github} className="logo" alt="icon for linked in"/></a>
-            <a href="mailto:jybasquez@yahoo.com" target="_blank" rel="noopener noreferrer"><img src={yahoo} className="logo" alt="icon for linked in"/></a>
+            <a href='https://github.com/jbasquez' target="_blank" rel="noopener noreferrer"><img src={github} className="logo" alt="icon for github"/></a>
+            <a href="mailto:jybasquez@yahoo.com" target="_blank" rel="noopener noreferrer"><img src={yahoo} className="logo" alt="icon for yahoo"/></a>
             </div>
         </div>
     )
