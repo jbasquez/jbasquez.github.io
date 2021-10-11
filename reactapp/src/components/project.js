0a1f90a29e068dd1104a12d6ff38bb761da21bc4
Joe Basquez <jybasquez@yahoo.com>
2021-10-11 16:01:57 -0400

    styled buttons for demo and repo

--- reactapp/src/components/project.js
+++ reactapp/src/components/project.js
@@ -16,7 +16,7 @@ function ProjectCard(props) {
               <strong>About:</strong> {props.about}
             </li>
               <a class="btn PrimaryBtn contact" href={props.active} target="_blank" rel="noopener noreferrer">Demo</a>
-              <a class="btn PrimaryBtn contact" href={props.repo} target="_blank" rel="noopener noreferrer">Repo</a>
+              <a class="btn PrimaryBtn contact btnSpace" href={props.repo} target="_blank" rel="noopener noreferrer">Repo</a>
           </ul>
         </div>
       </div>
