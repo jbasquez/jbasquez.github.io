6e178b17dbe12dfb0d92330524aa7a483e48c598
Joe Basquez <jybasquez@yahoo.com>
2021-10-11 17:19:02 -0400

    made linked in and github bring up blank new pages when clicked on in the contact card

--- reactapp/src/components/footer.js
+++ reactapp/src/components/footer.js
@@ -8,9 +8,9 @@ function Footer() {
             <section className="contact-info bg textColor">
               <ul className="noBullet">
                 <li><strong>Email: </strong> <a className="contact textColor" href="mailto:jybasquez@yahoo.com">jybasquez@yahoo.com</a></li>
-                <li><strong>Github: </strong> <a className="contact textColor" href="https://github.com/jbasquez">jbasquez</a></li>
+                <li><strong>Github: </strong> <a className="contact textColor" href="https://github.com/jbasquez" target="_blank" rel="noopener noreferrer">jbasquez</a></li>
                 <li><strong>Portfolio: </strong> <a className="contact textColor" href="portfolio.html">Click Here</a></li>
-                <li><strong>LinkedIn: </strong> <a className="contact textColor" href="https://www.linkedin.com/in/joseph-basquez-a1ab6a1b5/">Click Here</a></li>
+                <li><strong>LinkedIn: </strong> <a className="contact textColor" href="https://www.linkedin.com/in/joseph-basquez-a1ab6a1b5/" target="_blank" rel="noopener noreferrer">Click Here</a></li>
                 <li><strong>Phone Number: </strong> <a className="contact textColor" href="tel:+19103220946">910-322-0946</a></li>
               </ul>
             </section>
