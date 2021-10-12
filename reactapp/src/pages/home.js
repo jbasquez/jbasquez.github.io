9a6e49a73c1326946decf3777d38d271015bdce6
Joe Basquez <jybasquez@yahoo.com>
2021-10-11 20:14:18 -0400

    added in some movement on loading of ittems with movement of page by user

--- reactapp/src/pages/home.js
+++ reactapp/src/pages/home.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import"../scss/style.css";
 import selfie from "../img/selfie.png";
 import ProjectCard from '../components/project';
 import project from '../project.json';
 import Footer from '../components/footer';
 import SocialMediaIcons from '../components/icons';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 function Home(){
+      useEffect(() => {
+        Aos.init({duration: 3000});
+      }, []);
         return(
           <div>
             <div className="container">
               <div id="main-bio">
 
-                <h1 className="textColor">Joe Basquez</h1>
+                <h1 data-aos="fade-down" className="textColor">Joe Basquez</h1>
                 <div className="row">
 
-                  <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 bg textColor bottom">
+                  <div data-aos="fade-up" className="col-lg-6 col-md-6 col-sm-6 col-xs-12 bg textColor bottom">
                     <img src={selfie} className="img-fluid space" alt="selfie of website creator"/>
                     <h3>Bio:</h3>
                     <p>Highly motivated, hard working individual, with a passsion for front end and back end development. Do a good deed and learn something new will make sure no day is wasted.</p>
                   </div>
 
-                  <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 mainHome textColor">
+                  <div data-aos="fade-up" className="col-lg-6 col-md-6 col-sm-6 col-xs-12 mainHome textColor">
                   <h3 className="center">Social Media</h3>
                     <SocialMediaIcons />
                     <h3>Projects</h3>
