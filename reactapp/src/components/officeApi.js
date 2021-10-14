bd16384fb2f3f8fd5b6ca82b0e6aa4bd94104700
Joe Basquez <jybasquez@yahoo.com>
2021-10-14 18:51:29 -0400

    Office Api working and rendering

---
+++ reactapp/src/components/officeApi.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default class FetchRandomQuote extends React.Component {
+  state = {
+    loading: true,
+    quote: null
+  };
+
+  async componentDidMount() {
+    const url = "https://www.officeapi.dev/api/quotes/random";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ quote: data.data.content, loading: false });
+    console.log(data.data.content);
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <div>loading...</div>;
+    }
+
+    if (!this.state.quote) {
+      return <div>didn't get a quote</div>;
+    }
+
+    return (
+      <div>
+        <div>{this.state.quote}</div>
+      </div>
+    );
+  }
+}
--- reactapp/src/pages/home.js
+++ reactapp/src/pages/home.js
@@ -7,6 +7,7 @@ import Footer from '../components/footer';
 import SocialMediaIcons from '../components/icons';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
+import FetchRandomQuote from '../components/officeApi';
 
 function Home(){
       useEffect(() => {
@@ -24,11 +25,9 @@ function Home(){
                     <img src={selfie} className="img-fluid space" alt="selfie of website creator"/>
                     <h3 className="headers">Bio:</h3>
                     <p className="textColor">Highly motivated, hard working individual, with a passsion for Software Development. I enjoy agile working environments, see every challenge as an opportunity to learn, great at reading and understanding documentation, and have many skills such as photo and video editing.</p>
+                    <h2>Office Api Quote Generator</h2>
+                    <FetchRandomQuote />
                   </div>
-                  <div data-aos="fade-right" className="col-lg-6 col-md-6 col-sm-12 col-xs-12 bg textColor bottom">
-                    <h2>Random office quote</h2>
-                  </div>
-
                   <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 mainHome textColor">
                   <div data-aos="fade-left">
                   <h3 className="center headers">Social Media</h3>
