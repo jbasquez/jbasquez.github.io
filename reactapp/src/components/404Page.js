914b061330cd4c7ad435ded1bbd0ca232a4d9c49
Joe Basquez <jybasquez@yahoo.com>
2021-10-15 08:19:47 -0400

    styling 404

--- reactapp/src/components/404Page.js
+++ reactapp/src/components/404Page.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NoMatch =({history}) =>{
     return(
-        <div className="bg expand">
+        <div className="bg expand textColor">
             <h2>404Page</h2>
             <p>Redirecting to <span style={{color:'dodgerblue',cursor:'pointer'}} onClick={() => history.push('/')}>Login Page</span></p>
         </div>
