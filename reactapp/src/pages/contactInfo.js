f1ee3ed2dabf64aaabb14140facf3235b3f943ec
Joe Basquez <jybasquez@yahoo.com>
2021-10-16 09:08:13 -0400

    resizing for medium and small

--- reactapp/src/pages/contactInfo.js
+++ reactapp/src/pages/contactInfo.js
@@ -5,7 +5,7 @@ import"../scss/style.css";
 function ContactCard(){
     return(
         <div className="sizing">
-            <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 textColor mid">
+            <div className="col-lg-6 col-md-8 col-sm-10 col-xs-12 textColor mid">
                 <h2>Contact Me Now!</h2>
                 <SocialMediaIcons />
                 <div className="mar wid">
