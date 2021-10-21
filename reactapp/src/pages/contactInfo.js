7e4c094b7f414ca09c09ff08f10d0e9ba778d243
Joe Basquez <jybasquez@yahoo.com>
2021-10-21 08:21:01 -0400

    setting up interest

--- reactapp/src/pages/contactInfo.js
+++ reactapp/src/pages/contactInfo.js
@@ -5,11 +5,28 @@ import"../scss/style.css";
 function ContactCard(){
     return(
         <div className="sizing">
-            <div className="col-lg-6 col-md-8 col-sm-10 col-xs-12 textColor mid">
-                <h2>Contact Me Now!</h2>
-                <SocialMediaIcons />
-                <div className="mar wid">
-                    <Footer />
+            <div className="row">
+                <div className="col-lg-6 col-md-8 col-sm-10 col-xs-12 textColor mid">
+                    <h2>Contact Me Now!</h2>
+                    <SocialMediaIcons />
+                    <div className="mar wid">
+                        <Footer />
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+            <h2 className="textColor">Interest</h2>
+                <div className="col-lg-6 col-md-6 col-sm-10 col-xs-12 textColor mid">
+                    <SocialMediaIcons />
+                    <div className="mar wid">
+                        <Footer />
+                    </div>
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-10 col-xs-12 textColor mid">
+                    <SocialMediaIcons />
+                    <div className="mar wid">
+                        <Footer />
+                    </div>
                 </div>
             </div>
         </div>
